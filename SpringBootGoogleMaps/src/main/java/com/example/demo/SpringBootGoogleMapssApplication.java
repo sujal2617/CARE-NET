b92289eb8c314e01394fe1a5cccd14f2bac5472e
Sujal Shirish Pattewar <142489355+sujal2617@users.noreply.github.com>
2024-12-08 05:25:14 +0530

    Add files via upload
    google maps commit

---
+++ SpringBootGoogleMaps/src/main/java/com/example/demo/SpringBootGoogleMapssApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleMapssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoogleMapssApplication.class, args);
+	}
+
+}
